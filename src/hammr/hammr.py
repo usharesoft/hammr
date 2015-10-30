dc639f12c5576ee7317661b1070c3d0a30297971
Joris Bremond <joris.bremond@usharesoft.com>
2015-10-30 15:21:03 +0100

    Changinf/Adding licensing header

--- src/hammr/hammr.py
+++ src/hammr/hammr.py
@@ -1,6 +1,17 @@
-'''
-    hammr
-'''
+# Copyright 2007-2015 UShareSoft SAS, All rights reserved
+#
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 try:
     from termcolor import colored
@@ -28,10 +39,6 @@ from ussclicore.utils import printer
 import commands
 from uforge.application import Api
 from utils import *
-
-
-__author__ = "UShareSoft"
-__license__ = "Apache License 2.0"
 
 class CmdBuilder(object):
     @staticmethod
