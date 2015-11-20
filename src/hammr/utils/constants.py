3f69a12ca27074e84d686ba0a912e3d572a7c11a
Joris Bremond <joris.bremond@usharesoft.com>
2015-11-20 11:05:32 +0100

    Increase timeout value
    Conflicts:
    	src/hammr/utils/constants.py

--- src/hammr/utils/constants.py
+++ src/hammr/utils/constants.py
@@ -22,7 +22,7 @@ VERSION="0.3.0.0"
 
 
 TMP_WORKING_DIR=tempfile.gettempdir() + os.sep + "hammr-" + str(os.getpid())
-HTTP_TIMEOUT=20
+HTTP_TIMEOUT=30
 
 TEMPLATE_JSON_FILE_NAME="template.json"
 TEMPLATE_JSON_NEW_FILE_NAME="template.json"
