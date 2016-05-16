d57200430d4ca356f7c810cbdca2758705e4e7eb
jgweir <james.weir@usharesoft.com>
2016-05-16 18:00:06 +0200

    Added pdf creation for windows systems (make.bat) -- note for Linux, use latexpdf

--- documentation/en/make.bat
+++ documentation/en/make.bat
@@ -38,6 +38,7 @@ if "%1" == "help" (
 	echo.  linkcheck  to check all external links for integrity
 	echo.  doctest    to run all doctests embedded in the documentation if enabled
 	echo.  coverage   to run coverage check of the documentation if enabled
+	echo.  pdf        to make pdf file
 	goto end
 )
 
@@ -257,6 +258,13 @@ if "%1" == "pseudoxml" (
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished. The pseudo-XML files are in %BUILDDIR%/pseudoxml.
+	goto end
+)
+if "%1" == "pdf" (
+	%SPHINXBUILD% -b pdf %ALLSPHINXOPTS% %BUILDDIR%/pdf
+	if errorlevel 1 exit /b 1
+	echo.
+	echo.Build finished. The pdf files are in %BUILDDIR%/pdf.
 	goto end
 )
 
