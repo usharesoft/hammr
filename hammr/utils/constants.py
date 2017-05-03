351b3291a8d30e95bda4d5a16cb867b489585591
Ludovic Queiroga <ludovic.queiroga@usharesoft.com>
2017-05-03 08:36:39 +0200

    Hammr release new version 3.7.0.5-RC4

--- hammr/utils/constants.py
+++ hammr/utils/constants.py
@@ -18,7 +18,7 @@ __author__="UShareSoft"
 import os
 import tempfile
 
-VERSION="3.7.0.5-RC1"
+VERSION="3.7.0.5-RC4"
 
 
 TMP_WORKING_DIR=tempfile.gettempdir() + os.sep + "hammr-" + str(os.getpid())
