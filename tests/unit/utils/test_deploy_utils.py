086f2af5ee0607f1c82e39a6cb4b5f741d10a445
Alice Albano <alice.albano@usharesoft.com>
2017-07-03 12:08:30 +0200

    Refactor unit tests

--- tests/unit/utils/test_deploy_utils.py
+++ tests/unit/utils/test_deploy_utils.py
@@ -30,11 +30,8 @@ class TestDeploy(TestCase):
     @patch('uforge.application.Api._Users._Appliances._Images._Pimages._Deploys.Deploy')
     def test_do_deploy_return_2_when_imageId_is_None(self, mock_api_deploy):
         # given
-        i = hammr.commands.image.Image()
-        i.api = Api("url", username="username", password="password", headers=None,
-                    disable_ssl_certificate_validation=False, timeout=constants.HTTP_TIMEOUT)
-        i.login = "login"
-        i.password = "password"
+        i = self.prepare_image()
+
         deployment = self.get_deployment()
         mock_api_deploy.return_value = deployment
         args = self.prepare_image_deploy_command(None)
@@ -50,52 +47,18 @@ class TestDeploy(TestCase):
     @patch('uforge.application.Api._Users._Appliances._Images._Pimages._Deploys.Deploy')
     @patch('uforge.application.Api._Users._Appliances.Get')
     @patch('uforge.application.Api._Users._Deployments.Get')
-    def test_do_deploy_return_0_when_status_is_running(self, mock_depoyment_get, mock_app_get, mock_api_deploy, mock_get_deploy_status, mock_api_pimg_getall):
+    def test_do_deploy_return_0_when_status_is_running(self, mock_get_deployment, mock_app_get, mock_api_deploy, mock_get_deploy_status, mock_api_pimg_getall):
         # given
-        i = hammr.commands.image.Image()
-        i.api = Api("url", username="username", password="password", headers=None,
-                    disable_ssl_certificate_validation=False, timeout=constants.HTTP_TIMEOUT)
-        i.login = "login"
-        i.password = "password"
-        deployment = self.get_deployment()
-        mock_api_deploy.return_value = deployment
-        mock_depoyment_get.return_value = deployment
+        i = self.prepare_image()
 
+        self.prepare_mock_deploy(mock_get_deployment, mock_api_deploy)
+        self.prepare_mock_app_get(mock_app_get)
 
-        newAppliance = Appliance()
-        mock_app_get.return_value = newAppliance
-
-
-        new_pimages = uforge.publishImages()
-        new_pimages.publishImages = pyxb.BIND()
-
-        newImage = PublishImageAws()
-        newImage.dbId = 1234
-        newImage.imageUri = 'users/guest/appliances/5/images/1234'
-        newImage.applianceUri = 'users/guest/appliances/5'
-        newImage.fileSize = 0
-        newImage.size = 0
-        newImage.name = "test"
-        newImage.status = "complete"
-        newImage.status.complete = True
-        newImage.targetFormat = uforge.targetFormat()
-        newImage.targetFormat.name = "test"
-        newImage.created = datetime.datetime.now()
-        newImage.compress = True
-
-        new_pimages.publishImages.append(newImage)
-
-
+        new_pimages = self.prepare_pimages()
         mock_api_pimg_getall.return_value = new_pimages
 
         args = self.prepare_image_deploy_command(1234)
-
-        statusStarting = OpStatus()
-        statusStarting.message = "starting"
-        statusRunning = OpStatus()
-        statusRunning.message = "running"
-        mock_get_deploy_status.side_effect = [statusStarting, statusStarting, statusRunning]
-
+        self.prepare_mock_deploy_status(mock_get_deploy_status)
 
         # when
         deploy_return = i.do_deploy(args)
@@ -127,3 +90,50 @@ class TestDeploy(TestCase):
     def prepare_image_deploy_command(self, id):
          args = "--id %s --name DeploymentName" % (id)
          return args
+
+    def prepare_image(self):
+        i = hammr.commands.image.Image()
+        i.api = Api("url", username="username", password="password", headers=None,
+                    disable_ssl_certificate_validation=False, timeout=constants.HTTP_TIMEOUT)
+        i.login = "login"
+        i.password = "password"
+        return i
+
+    def prepare_pimages(self):
+        new_pimages = uforge.publishImages()
+        new_pimages.publishImages = pyxb.BIND()
+
+        newImage = PublishImageAws()
+        newImage.dbId = 1234
+        newImage.imageUri = 'users/guest/appliances/5/images/1234'
+        newImage.applianceUri = 'users/guest/appliances/5'
+        newImage.fileSize = 0
+        newImage.size = 0
+        newImage.name = "test"
+        newImage.status = "complete"
+        newImage.status.complete = True
+        newImage.targetFormat = uforge.targetFormat()
+        newImage.targetFormat.name = "test"
+        newImage.created = datetime.datetime.now()
+        newImage.compress = True
+
+        new_pimages.publishImages.append(newImage)
+
+        return new_pimages
+
+    def prepare_mock_deploy_status(self, mock_get_deploy_status):
+        # The mock will return statusStarting on first and second calls, and statusRunning on third call
+        statusStarting = OpStatus()
+        statusStarting.message = "starting"
+        statusRunning = OpStatus()
+        statusRunning.message = "running"
+        mock_get_deploy_status.side_effect = [statusStarting, statusStarting, statusRunning]
+
+    def prepare_mock_deploy(self, mock_get_deployment, mock_api_deploy):
+        deployment = self.get_deployment()
+        mock_api_deploy.return_value = deployment
+        mock_get_deployment.return_value = deployment
+
+    def prepare_mock_app_get(self, mock_app_get):
+        newAppliance = Appliance()
+        mock_app_get.return_value = newAppliance
