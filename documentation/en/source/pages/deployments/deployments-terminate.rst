25df5de8355b2f192837f7613fd3d299c387d40c
AliceAlbano <AliceAlbano@users.noreply.github.com>
2017-08-22 09:50:32 +0200

    Add Deploy list and deploy terminate commands (#240)
    * add a command to list deployments
    * Implements terminate for deploy
    * Add documentation for list and terminate

---
+++ documentation/en/source/pages/deployments/deployments-terminate.rst
@@ -0,0 +1,20 @@
+.. Copyright (c) 2007-2016 UShareSoft, All rights reserved
+
+.. _deployments-terminate:
+
+Terminating a deployment
+========================
+
+You terminate a deployment by running ``deploy terminate``:
+
+.. code-block:: shell
+
+	$ hammr deploy terminate --id <your-id>
+        Do you really want to delete deployment with id t0id1exb3d [Y/n] 
+        Y
+        Deployment is stopping
+        |##############################################################|
+        OK: Deployment terminated
+
+
+This command will delete your deployment on the targeted cloud. You cand find the ID of the deployment by using the command ``deploy list``.
---
+++ tests/unit/commands/deploy/test_deploy_list.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Copyright 2007-2016 UShareSoft SAS, All rights reserved
+#
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from unittest import TestCase
+
+import pyxb
+from mock import patch
+from uforge.application import Api
+from uforge.objects import uforge
+from uforge.objects.uforge import *
+
+from hammr.commands.deploy import deploy
+from hammr.utils import constants
+
+class TestDeployList(TestCase):
+    # URL: users/{uid}/deployments/{did}/status
+    @patch('uforge.application.Api._Users._Deployments.Getall')
+    @patch('texttable.Texttable.add_row')
+    def test_do_list_gives_correct_number_of_deployments(self, mock_table_add_row, mock_api_deployments_getall):
+        # given
+        i = deploy.Deploy()
+        i.api = Api("url", username="username", password="password", headers=None,
+                    disable_ssl_certificate_validation=False, timeout=constants.HTTP_TIMEOUT)
+        i.login = "login"
+        i.password = "password"
+        self.create_deployments(mock_api_deployments_getall)
+
+        # when
+        i.do_list(None)
+
+        # then
+        self.assertEquals(mock_table_add_row.call_count, 1)
+
+    def create_deployments(self,mock_api_deployments_getall):
+        new_deployments = uforge.deployments()
+        new_deployments.deployments = pyxb.BIND()
+
+        newdeployment = self.create_deployment()
+        new_deployments.deployments.append(newdeployment)
+
+        mock_api_deployments_getall.return_value = new_deployments
+
+    def create_deployment(self):
+        deployment = Deployment()
+        deployment.name = "DeploymentName"
+        deployment.applicationId = "id123456789"
+        deployment.state = "running"
+
+        myinstance = Instance()
+        myinstance.cores = "1"
+        myinstance.memory = "1024"
+        myinstance.hostname = "example.com"
+        myLocation = Location()
+        myLocation.provider = "myprovider"
+        myinstance.location = myLocation
+
+        deployment.instances = pyxb.BIND()
+        deployment.instances._ExpandedName = pyxb.namespace.ExpandedName(Namespace, 'Instances')
+        deployment.instances.append(myinstance)
+
+        return deployment
