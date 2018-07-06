3cc744c40bf1decc027d332deb75981e001c888f
Yann Blanc <yann.blanc@usharesoft.com>
2018-07-06 13:27:21 +0200

    DEVENV-185 As a dev, I want to hammr CI run PyLint on each commit PyLint will run on every PR with only new code PyLint will be runnable on local environment and will inspect staged changes

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -41,7 +41,7 @@ def get_template_id(template, name):
         id = cmd.read().rstrip()
         os.remove("stdout_file")
         return id
-    
+
 def get_account_id(account, name):
         stdout = sys.stdout
         sys.stdout = open('stdout_file', 'w')
@@ -51,7 +51,7 @@ def get_account_id(account, name):
         id = cmd.read().rstrip()
         os.remove("stdout_file")
         return id
-    
+
 def get_image_id(image, name):
         stdout = sys.stdout
         sys.stdout = open('stdout_file', 'w')
@@ -363,5 +363,3 @@ if __name__ == '__main__':
                 unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
         else:
                 unittest.main()
-
-
