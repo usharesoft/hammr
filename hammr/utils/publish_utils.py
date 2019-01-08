9fa7534a50f14219322307b281f117ae57e8b3a9
Vincent Powazny <vincent.powazny@usharesoft.com>
2019-01-08 14:17:33 +0100

    Update company copyright message from 2018 to 2019

--- hammr/utils/publish_utils.py
+++ hammr/utils/publish_utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2007-2018 UShareSoft, All rights reserved
+# Copyright (c) 2007-2019 UShareSoft, All rights reserved
 #
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
