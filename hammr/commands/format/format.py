65afe72b602457a79194a998b71dc1b9c81b7ad7
Vincent Powazny <vincent.powazny@usharesoft.com>
2019-01-10 15:28:22 +0100

    Update company copyright message from 2018 to 2019

--- hammr/commands/format/format.py
+++ hammr/commands/format/format.py
@@ -1,6 +1,6 @@
 __author__="UShareSoft"
 
-# Copyright 2007-2015 UShareSoft SAS, All rights reserved
+# Copyright 2007-2019 UShareSoft SAS, All rights reserved
 #
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
