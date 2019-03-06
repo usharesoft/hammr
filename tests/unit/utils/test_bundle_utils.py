3fcce87a433a85a2bed4ff26569c27482827a8d2
Vincent Powazny <vincent.powazny@usharesoft.com>
2019-03-06 15:19:15 +0100

    Revert "Add unit tests on recursivelyAppendToArchive method"
    This reverts commit 02d54e27a04cb382f458546c3762c561a59081ca.

--- tests/unit/utils/test_bundle_utils.py
+++ tests/unit/utils/test_bundle_utils.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import
 __author__ = 'UshareSoft'
 
 import unittest
@@ -8,14 +7,13 @@ from mock import patch
 
 from ussclicore.utils import generics_utils
 from hammr.utils import bundle_utils
-from tests.unit.utils.file_utils import find_relative_path_for
 
 class TestFiles(unittest.TestCase):
 
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_no_name(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleWithoutName.json")
+        jsonPath = "tests/integration/data/bundle/bundleWithoutName.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -25,7 +23,7 @@ class TestFiles(unittest.TestCase):
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_no_version(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleWithoutVersion.json")
+        jsonPath = "tests/integration/data/bundle/bundleWithoutVersion.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -35,7 +33,7 @@ class TestFiles(unittest.TestCase):
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_no_files(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleWithoutFiles.json")
+        jsonPath = "tests/integration/data/bundle/bundleWithoutFiles.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -45,7 +43,7 @@ class TestFiles(unittest.TestCase):
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_files_no_name(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleFilesWithoutName.json")
+        jsonPath = "tests/integration/data/bundle/bundleFilesWithoutName.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -55,7 +53,7 @@ class TestFiles(unittest.TestCase):
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_files_no_source(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleFilesWithoutSource.json")
+        jsonPath = "tests/integration/data/bundle/bundleFilesWithoutSource.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -65,7 +63,7 @@ class TestFiles(unittest.TestCase):
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_tag_softwarefile_and_bootorder(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleFilesTagSoftwareFileKeyBootOrder.json")
+        jsonPath = "tests/integration/data/bundle/bundleFilesTagSoftwareFileKeyBootOrder.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -75,7 +73,7 @@ class TestFiles(unittest.TestCase):
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_tag_bootscript_and_rights(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleFilesTagBootScriptKeyRights.json")
+        jsonPath = "tests/integration/data/bundle/bundleFilesTagBootScriptKeyRights.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -85,7 +83,7 @@ class TestFiles(unittest.TestCase):
     @patch("ussclicore.utils.printer.out")
     def test_check_bundle_should_failed_when_tag_ospkg_in_directory(self, mock_method):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleFilesTagOSPkgInDirectory.json")
+        jsonPath = "tests/integration/data/bundle/bundleFilesTagOSPkgInDirectory.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -94,7 +92,7 @@ class TestFiles(unittest.TestCase):
 
     def test_check_bundle_should_succeed_when_no_restrictionRule(self):
         # Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleWithoutRestrictionRule.json")
+        jsonPath = "tests/integration/data/bundle/bundleWithoutRestrictionRule.json"
         # When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -103,7 +101,7 @@ class TestFiles(unittest.TestCase):
 
     def test_check_bundle_should_succeed_when_empty_restrictionRule(self):
         # Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleWithEmptyRestrictionRule.json")
+        jsonPath = "tests/integration/data/bundle/bundleWithEmptyRestrictionRule.json"
         # When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle_utils.check_bundle(bundle)
@@ -112,90 +110,12 @@ class TestFiles(unittest.TestCase):
 
     def test_check_bundle_should_succeed(self):
         #Given
-        jsonPath = find_relative_path_for("tests/integration/data/bundle/bundleFull.json")
+        jsonPath = "tests/integration/data/bundle/bundleFull.json"
         #When
         bundle = generics_utils.check_json_syntax(jsonPath)
         bundle = bundle_utils.check_bundle(bundle)
         #Then
         self.assertIsNotNone(bundle)
-
-    def test_recursivelyAppendToArchive_should_failed_when_two_files_have_same_archive_path(self):
-        # Given
-        bundle = { 'name': 'MyBundle', 'version': '1.0' }
-        parent_dir = ""
-        check_list = []
-        archive_files = []
-        files = {
-            'name': 'myDirectory',
-            'source': 'tests/integration/data/directoryTest',
-            'tag': 'softwarefile',
-            'destination': '/usr/local/myBundle',
-            'files': [
-                {
-                    "name": "file.txt",
-                    "source": "tests/integration/data/directoryTest/file1of3.txt"
-                },
-                {
-                    "name": "file.txt",
-                    "source": "tests/integration/data/directoryTest/file2of3.txt"
-                }
-            ]
-        }
-        # When
-        with self.assertRaises(ValueError) as context_manager:
-            bundle_utils.recursivelyAppendToArchive(bundle, files, parent_dir, check_list, archive_files)
-
-        # Then
-        self.assertEqual(
-            context_manager.exception.message,
-            "Cannot have identical files in the bundles section: bundles/MyBundle/1.0/myDirectory/file.txt from tests/integration/data/directoryTest/file2of3.txt"
-        )
-
-    def test_recursivelyAppendToArchive_should_succeed_when_several_files_have_same_source(self):
-        # Given
-        bundle = { 'name': 'MyBundle', 'version': '1.0' }
-        parent_dir = ""
-        check_list = []
-        archive_files = []
-        files = {
-            'name': 'myDirectory',
-            'source': 'tests/integration/data/directoryTest',
-            'tag': 'softwarefile',
-            'destination': '/usr/local/myBundle',
-            'files': [
-                {
-                    'name': 'file1.txt',
-                    'source': 'tests/integration/data/directoryTest/file1of3.txt'
-                },
-                {
-                    'name': 'file2.txt',
-                    'source': 'tests/integration/data/directoryTest/file1of3.txt'
-                },
-                {
-                    'name': 'pkg1.rpm',
-                    'source': 'http://myServer.com/pkg1/download',
-                    'install': 'true'
-                },
-                {
-                    'name': 'pkg2.rpm',
-                    'source': 'http://myServer.com/pkg2/download',
-                    'install': 'true'
-                }
-            ]
-        }
-        # When
-        r_check_list, r_archive_files = bundle_utils.recursivelyAppendToArchive(bundle, files, parent_dir, check_list, archive_files)
-
-        # Then
-        self.assertEqual(archive_files, [
-            ['bundles/MyBundle/1.0/myDirectory', 'tests/integration/data/directoryTest'],
-            ['bundles/MyBundle/1.0/myDirectory/file1.txt', 'tests/integration/data/directoryTest/file1of3.txt'],
-            ['bundles/MyBundle/1.0/myDirectory/file2.txt', 'tests/integration/data/directoryTest/file1of3.txt'],
-            ['bundles/MyBundle/1.0/myDirectory/pkg1.rpm','http://myServer.com/pkg1/download'],
-            ['bundles/MyBundle/1.0/myDirectory/pkg2.rpm', 'http://myServer.com/pkg2/download']
-        ])
-        self.assertEqual(r_archive_files, archive_files)
-
 
 if __name__ == '__main__':
     unittest.main()
